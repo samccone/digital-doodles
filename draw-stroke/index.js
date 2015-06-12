1a6ff831ca9903557ffa29affae6fa545a25e730
Sam Saccone <samccone@gmail.com>
2015-06-12 16:49:40 -0400

    move stroke to new project

---
+++ draw-stroke/index.js
@@ -0,0 +1,16 @@
+var canvas = document.createElement('canvas');
+canvas.setAttribute('width', window.innerWidth);
+canvas.setAttribute('height', window.innerHeight);
+
+var ctx = canvas.getContext('2d');
+document.body.appendChild(canvas);
+
+
+var paper = [{},{},{},{},{},
+             {},{},{},{},{},
+             {},{},{},{},{},
+             {},{},{},{},{},
+             {},{},{},{},{}];
+
+paper.width = 5;
+
